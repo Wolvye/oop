7effd44c6965c64755281a38b1785e0bd9733343
Wolvye <alainsoltau@googlemail.com>
2025-03-25 12:55:46 +0100

    Add project files.

---
+++ oop/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    internal class Car
+    {
+        public string Brand { get; private set; }
+        public string Model { get; private set; }
+        public int Year { get; private set; }
+
+        public Car(string brand, string model, int year)
+        {
+            Brand = brand; // Das Erste ist die Property der Klasse(wird großgeschrieben) und das zweite ist der Übergabeparameter, der vom Konstruktor kommt(deshalb auch kleingeschrieben)
+            Model = model;
+            Year = year;
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("HOOOONK!");
+        }
+    }
+}
---
+++ oop/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Honk();
+        }
+    }
+}
