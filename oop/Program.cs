025b16d2f5ec35771750468bb18041136996ca26
Wolvye <alainsoltau@googlemail.com>
2025-03-26 10:40:52 +0100

    Update

--- oop/Program.cs
+++ oop/Program.cs
@@ -4,8 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Car myCar = new Car();
+            Car myCar = new Car("Toyota","IrgendeinModell",2020);
             myCar.Honk();
+            Console.WriteLine(myCar.Brand);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
         }
     }
 }
